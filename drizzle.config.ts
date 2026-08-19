0ea50001c326c086423d6e98819307a8b6b9c551
kclamano <klamano23@gmail.com>
2026-08-19 15:44:25 +0800

    fix database migrations and notifications

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+dotenv.config({ path: ".env.production" });
+
+export default defineConfig({
+  schema: "./lib/db/schema.ts",
+  out: "./migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
